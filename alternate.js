f609044a633138bc1b9011212ff9eb22a82f006c
Daniel Levin <sporks@gmail.com>
2019-06-01 16:16:21 -0400

    fixed if input all uppercase

--- alternate.js
+++ alternate.js
@@ -1,17 +1,16 @@
-const input = process.argv[2]
+const input = process.argv[2];
 
 function alternate(str) {
-    let ret = ""
+    let ret = "";
 	for(i=0; i<str.length; i++){
-		let temp = ""
         if(i%2 === 1){
-            ret += str[i].toUpperCase()
+            ret += str[i].toUpperCase();
         }
         else {
-            ret += str[i]
+            ret += str[i].toLowerCase();
         }
     }
-	return ret
+	return ret;
 }
 
-console.log(alternate(input))
+console.log(alternate(input));
